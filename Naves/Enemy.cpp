8f0c21c4b8f4e702d809b99892a703052a3de291
Alejandro Iglesias Calvo <47303435+UO247346@users.noreply.github.com>
2020-09-17 17:43:54 +0200

    Los enemigos se mueven a distintas velodidades aleatoriamente

--- Naves/Enemy.cpp
+++ Naves/Enemy.cpp
@@ -2,11 +2,12 @@
 
 Enemy::Enemy(float x, float y, Game* game)
 	: Actor("res/enemigo.png", x, y, 36, 40, game) {
-
-	vx = 1;
+	int VxRandom = 1 + (rand() % 12);
+	vx = VxRandom;
 }
 
 void Enemy::update() {
-	vx = -1;
-	x = x + vx;
+	
+	vx = (this->vx);
+	x = x - vx;
 }
