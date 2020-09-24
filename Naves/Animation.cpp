8723e59e77adca2ce1537c4129582f5136e70aad
Alejandro Iglesias Calvo <47303435+UO247346@users.noreply.github.com>
2020-09-24 16:10:57 +0200

    Estaba en la clase que no es

--- Naves/Animation.cpp
+++ Naves/Animation.cpp
@@ -29,3 +29,31 @@ Animation::Animation(string filename, float actorWidth, float actorHeight,
 	source.w = frameWidth;
 	source.h = frameHeigt;
 }
+bool Animation::update() {
+	updateTime++;
+	if (updateTime > updateFrecuence) {
+		updateTime = 0;
+		// Actualizar el frame
+		currentFrame++;
+		// Si lleva al ultimo frame vuelve al primero
+		if (currentFrame >= totalFrames) {
+			// Reiniciar es infinita
+			currentFrame = 0;
+		}
+	}
+	//Actualizar el rectangulo del source (siguiente frame)
+	source.x = currentFrame * frameWidth;
+	return false; // luego lo cambiamos
+}
+void Animation::draw(float x, float y) {
+	// Donde se va a pegar en el renderizador
+	SDL_Rect destination;
+	destination.x = x - actorWidth / 2;
+	destination.y = y - actorHeight / 2;
+	destination.w = actorWidth;
+	destination.h = actorHeight;
+	// Modificar para que la referencia sea el punto central
+
+	SDL_RenderCopyEx(game->renderer,
+		texture, &source, &destination, 0, NULL, SDL_FLIP_NONE);
+}
